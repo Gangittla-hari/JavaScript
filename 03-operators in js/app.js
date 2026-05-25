98ac994d764a71e7ace6540a19cae93af3d38c98
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-05-25 18:03:26 +0530

    Operators in js

---
+++ 03-operators in js/app.js
@@ -0,0 +1,25 @@
+//Arithmetic operators
+// let a = 10;
+// let b = 5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+//unary operators
+// let a = 10;
+// let b = 5;
+// console.log(a++); // output 10
+// console.log(++a); // 12
+
+// Assingment operators
+// let a = 10;
+// let b = 5;
+// b = a;
+// console.log(b); //10
+
+//Comparion operators
+let age = 18;
+console.log(age > 18); // false
