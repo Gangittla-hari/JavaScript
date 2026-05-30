582a0e4ba5b6cc4ec5060cb1ea74bf76521a2559
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-05-30 18:02:32 +0530

    updated code

---
+++ 06-else if statement/app.js
@@ -0,0 +1,27 @@
+// else if statement
+let age =23;
+if(age >= 18) {
+    console.log("you can vote");
+} else if (age < 18) {
+    console.log("you cannot vote");
+}
+
+// ex-2
+let mark = 80;
+if (mark >= 80) {
+    console.log("A+");
+} else if (mark >= 60) {
+    console.log("A");
+} else if (mark >= 30) {
+    console.log("B");
+} else if (mark < 30) {
+    console.log("F");
+}
+
+// ex-3
+let month = "April";
+if (month === "january")  {
+    console.log("winter is here");
+} else if (month === "April") {
+    console.log("summer is here");
+}
