6b0704c23e89e6140f0f7944969f248a3ef4ef61
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-05-30 17:51:12 +0530

    else if statement

--- 05-Practice Qa/app.js
+++ 05-Practice Qa/app.js
@@ -1,12 +1,16 @@
-let color =  "red";
+// let color =  "yellow";
 
-//Traffic light System
-if (color ==="red") {
-    console.log("stop! light color is red");
-}
-if (color ==="yellow") {
-    console.log("slow down. light color is yellow");
-}
-if (color ==="green") {
-    console.log("go! light color is green");
-}
+// //Traffic light System
+// if (color ==="red") {
+//     console.log("stop! light color is red");
+// }
+// if (color ==="yellow") {
+//     console.log("slow down. light color is yellow");
+// }
+// if (color ==="green") {
+//     console.log("go! light color is green");
+// } 
+
+// 
+
+
