bbb4724fca09725d485949c27a61fda880648443
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-02 20:57:15 +0530

    Nested if-else

---
+++ 09-Nested if-else/app.js
@@ -0,0 +1,12 @@
+let mark = 33;
+
+if (mark >= 33) {
+    console.log("pass");
+   if (mark >= 80) {
+    console.log("Grade : O");
+   } else {
+    console.log("Grade : A");
+   }
+} else {
+    console.log("Better luck next time");
+}
