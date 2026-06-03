bd1775416286e844a863d8c435a236778e39b600
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-03 17:37:48 +0530

    Logical OperLogical Operators

---
+++ 10-Logical Operators/app.js
@@ -0,0 +1,32 @@
+// and and &&
+// let marks = 90;
+
+// if (marks >= 33 && marks >= 80) {
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+// or ||
+// let marks = 90;
+
+// if (marks >= 33 || marks >= 80) {
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+// not !
+// let marks = 75;
+
+// if (!(marks < 33)) {
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+//combo 
+let marks = 75;
+
+if ((marks > 33 && marks <=80) || !false) {
+    console.log("pass");
+}
+
+
