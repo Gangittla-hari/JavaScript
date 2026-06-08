f5ee422cbd845cbbbf8a93709edf8c38b674c54d
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-09 00:49:23 +0530

    Method Chaining

--- 19-Methods with Arguments - indexOf/app.js
+++ 19-Methods with Arguments - indexOf/app.js
@@ -1,10 +1,14 @@
-let msg = "iloveGoogle";
+// let msg = "iloveGoogle";
 
-msg
-'iloveGoogle'
+// msg
+// 'iloveGoogle'
 
-msg.indexOff("love");
-1
+// msg.indexOff("love");
+// 1
 
-msg.indexOff("i");
-0
+// msg.indexOff("i");
+// 0
+
+// msg.indexOff("z");
+// -1 (not found)
+
