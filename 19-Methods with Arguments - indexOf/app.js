8890901c281dc111d21f8a5e8c7dfc0bd9621868
Gangittla Hariprasad <hariprasad20987@gmail.com>
2026-06-08 20:37:57 +0530

    Update app.js

--- 19-Methods with Arguments - indexOf/app.js
+++ 19-Methods with Arguments - indexOf/app.js
@@ -1,10 +1,13 @@
 let msg = "iloveGoogle";
 
-msg
-'iloveGoogle'
+// msg
+// 'iloveGoogle'
 
-msg.indexOff("love");
-1
+// msg.indexOff("love");
+// 1
 
-msg.indexOff("i");
-0
+// msg.indexOff("i");
+// 0
+
+// msg.indexOff("z");
+// -1 (not found)
