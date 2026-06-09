3704684456a3a9eff43dcaa7ed2b4859f7ccb53d
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-09 20:32:15 +0530

    Slice

---
+++ 21-Slice/app.js
@@ -0,0 +1,13 @@
+let msg ="HelloWorld";
+
+console.log(msg.slice(0,5)) //Hello
+console.log(msg.slice(5,10)) //World
+console.log(msg.slice(5,msg.length)) //World
+console.log(msg.slice(0,msg.length)) //HelloWorld
+console.log(msg.slice(7)) //rld
+console.log(msg.slice(-2)) //ld 10-2 =>8
+console.log(msg.slice(-0)) //World 
+
+
+
+
