6c3e3c5a25da5fbfb39c899e4d3a1781dda46b7c
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-12 23:05:40 +0530

    Practice

---
+++ 25-Practice Qa/README.md
@@ -0,0 +1,5 @@
+Q1. For the given start state of an array, change it to final form using methods.
+
+start: ['january', 'july', 'march', 'august']
+
+final: ['july', 'june', 'march', 'august']
---
+++ 25-Practice Qa/app.js
@@ -0,0 +1,27 @@
+let months = ["january", "july", "march", "august"];
+
+// Ans
+// months
+// (4) ['january', 'july', 'march', 'august']
+
+// months.shift();
+// 'january'
+
+// months
+// (3) ['july', 'march', 'august']
+
+// months.shift();
+// 'july'
+
+// months
+// (2) ['march', 'august']
+// months.unshift("june");
+// 3
+
+// months
+// (3) ['june', 'march', 'august']
+
+// months.unshift("july");
+// 4
+// months
+// (4) ['july', 'june', 'march', 'august']
