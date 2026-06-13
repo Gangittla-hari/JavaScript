974f683d75646a2349beb56bffb3504dd6c7e773
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-13 23:00:36 +0530

    indexof & includes method

---
+++ 26-indexof & includes method/app.js
@@ -0,0 +1,16 @@
+let primary =  ["red", "yellow", "blue"];
+
+primary
+(3) ['red', 'yellow', 'blue']
+
+primary.indexOf("red");
+0
+
+primary.indexOf("yellow");
+1
+
+primary.indexOf("green");
+-1
+
+primary.indexOf("Yellow");
+-1
