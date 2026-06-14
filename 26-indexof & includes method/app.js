5a284fd213c1597c6c0dc254c5c591ffa5ac9c35
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-14 22:02:38 +0530

    added code

--- 26-indexof & includes method/app.js
+++ 26-indexof & includes method/app.js
@@ -1,23 +1,43 @@
+// indexOf : return index of something
 let primary =  ["red", "yellow", "blue"];
 
-// indexOf : return index of something
+// primary
+// (3) ['red', 'yellow', 'blue']
+
+// primary.indexOf("red");
+// 0
+
+// primary.indexOf("yellow");
+// 1
+
+// primary.indexOf("green");
+// -1
+
+// primary.indexOf("Yellow");
+// -1
+
+// //includes: search for a value
+// primary.includes("green");
+// false
+// primary.includes("red");
+// true
+
+
+// concat : merge 2 array
+let secondary = ["orange", "green", "violet"];
+
+primary.concat(secondary);
+(6) ['red', 'yellow', 'blue', 'orange', 'green', 'violet']
+
+secondary
+(3) ['orange', 'green', 'violet']
+
 primary
 (3) ['red', 'yellow', 'blue']
 
-primary.indexOf("red");
-0
+secondary.concat(primary);
+(6) ['orange', 'green', 'violet', 'red', 'yellow', 'blue']
 
-primary.indexOf("yellow");
-1
-
-primary.indexOf("green");
--1
-
-primary.indexOf("Yellow");
--1
-
-//includes: search for a value
-primary.includes("green");
-false
-primary.includes("red");
-true
+//reverae: reverse an array
+primary.reverse();
+(3) ['blue', 'yellow', 'red']
