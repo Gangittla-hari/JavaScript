57478ceca7e09f0b8f4cd1cd7921ed9c90ce4ce0
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-14 23:15:51 +0530

    sort arrays

---
+++ 29-Sort in arrays/app.js
@@ -0,0 +1,26 @@
+//sort : sorts an array
+let color = ["red", "yellow", "blue", "orange", "pink", "white"];
+
+color
+(6) ['red', 'yellow', 'blue', 'orange', 'pink', 'white']
+
+color.sort();
+(6) ['blue', 'orange', 'pink', 'red', 'white', 'yellow']
+
+let chars = [ "b", "d", "c", "a"];
+undefined
+
+chars
+(4) ['b', 'd', 'c', 'a']
+
+chars.sort();
+(4) ['a', 'b', 'c', 'd']
+
+let marks =[99,54,24,94,100];
+undefined
+
+marks
+(5) [99, 54, 24, 94, 100]
+
+marks.sort();
+(5) [100, 24, 54, 94, 99]
