07ad1115cd33c73f69ce49e2d26f37cd4842b002
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-15 22:06:38 +0530

    Nested Arrays

---
+++ 33-Nested Arrays/app.js
@@ -0,0 +1,26 @@
+//array of arryas
+
+let nums = [ [1, 2], [3, 4], [4, 5] ];
+undefined
+
+let nums =  [ [2, 4], [3, 6], [4, 8] ];
+undefined
+
+nums
+(3) [Array(2), Array(2), Array(2)]
+0: (2) [2, 4]
+1: (2) [3, 6]
+2: (2) [4, 8]
+length: 3
+
+nums [0][0]
+2
+
+nums[0][1]
+4
+
+nums[1][1]
+6
+
+nums[2][0]
+4
