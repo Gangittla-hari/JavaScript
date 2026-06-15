a0cb19d72263ff8ebe316db2d52d10a6d336cac0
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-15 21:51:43 +0530

    Constant Arrays

--- 31-Array References/app.js
+++ 31-Array References/app.js
@@ -1,29 +1,29 @@
 let arr =  ['a', 'b', 'c',]; 
-undefined
+// undefined
 
-let arrCopy = arr;
-undefined
+// let arrCopy = arr;
+// undefined
 
-arr == arrCopy
-true
+// arr == arrCopy
+// true
 
-arr === arrCopy
-true
+// arr === arrCopy
+// true
 
-arr.push('d');
-4
+// arr.push('d');
+// 4
 
-arr
-(4) ['a', 'b', 'c', 'd']
+// arr
+// (4) ['a', 'b', 'c', 'd']
 
-arrCopy
-(4) ['a', 'b', 'c', 'd']
+// arrCopy
+// (4) ['a', 'b', 'c', 'd']
 
-arrCopy.pop();
-'d'
+// arrCopy.pop();
+// 'd'
 
-arrCopy
-(3) ['a', 'b', 'c']
+// arrCopy
+// (3) ['a', 'b', 'c']
 
-arr
-(3) ['a', 'b', 'c']
+// arr
+// (3) ['a', 'b', 'c']
---
+++ 32-Constant Arrays/app.js
@@ -0,0 +1,20 @@
+const arr = [1,2,3,4];
+// const arr = [1,2,3];
+// undefined
+
+// arr
+// (3) [1, 2, 3]
+
+// arr.push(4);
+// 4
+
+// arr
+// (4) [1, 2, 3, 4]
+
+// arr.pop();
+// 4
+
+// arr
+// (3) [1, 2, 3]
+
+// arr =  [1,2,3];//Assignment to constant variable.
