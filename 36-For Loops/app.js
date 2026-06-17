c324ee582bc1cc43b643506836a8584a69c72f38
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-17 16:53:04 +0530

    added code

--- 36-For Loops/app.js
+++ 36-For Loops/app.js
@@ -4,6 +4,11 @@ console.log("3");
 console.log("4");
 console.log("5");
 
-for(let i=1; i<=5; i++) {
+// for(let i=1; i<=5; i++) {
+//     console.log(i);
+// }
+
+
+for(let i=5; i>=1; i--) {
     console.log(i);
 }
