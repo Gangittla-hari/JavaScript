bb5ba92a72f631044602883bcfa972dd204db3d8
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-22 21:28:38 +0530

    while loop

---
+++ 42-While loop/app.js
@@ -0,0 +1,30 @@
+let i=1
+
+while (i <=5) {
+    console.log(i);
+    i++;
+}
+
+// console.log("backward")
+let i=5;
+
+while (i >=1) {
+    console.log(i);
+    i--;
+}
+
+//0 to 20 numbers print
+let i = 0;
+
+while (i <=20) {
+    console.log(i);
+    i++;
+}
+
+//even numbers
+let i = 0
+
+while (i <=20) {
+    console.log(i);
+    i = i+2;
+}
