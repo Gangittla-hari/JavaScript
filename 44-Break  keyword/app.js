905449d10cf47008b369e8ccf689ab6558b08fe3
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-24 00:02:40 +0530

    Break Keyword

---
+++ 44-Break  keyword/app.js
@@ -0,0 +1,17 @@
+// let i=1;
+// while(i<=5) {
+//     console.log(i);
+//     i++;
+// }
+
+// Break keyword we used  break at 3
+let i=1;
+while(i<=5) {
+    if(i == 3) {
+        break;
+    }
+    console.log(i);
+    i++;
+}
+
+console.log("we used  break at 3");
