7df21b2c00ec857b21ef17f7917afb0426b92626
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-25 23:32:41 +0530

    added code

--- 46-Loops with Nested Arrays/app.js
+++ 46-Loops with Nested Arrays/app.js
@@ -1,11 +1,20 @@
-let heroes = [
-    ["ironmsn", "spiderman", "thor"],
-    ["superman", "wonder woman", "flash"]
-]
+// let heroes = [
+//     ["ironmsn", "spiderman", "thor"],
+//     ["superman", "wonder woman", "flash"]
+// ]
 
-for(let i=0;  i<heroes.length; i++) {
-    console.log(i,heroes[i], heroes[i].length);
-    for(let j=0; j< heroes[i].length; j++ ){
-        console.log(`j=${j}, ${heroes[i][j]}`)
-    }
+// for(let i=0;  i<heroes.length; i++) {
+//     console.log(i,heroes[i], heroes[i].length);
+//     for(let j=0; j< heroes[i].length; j++ ){
+//         console.log(`j=${j}, ${heroes[i][j]}`)
+//     }
+// }
+
+let student = [ ["aman", 95], ["sradha",  67], ["hari",100] ];
+  
+for(let i=0; i<student.length; i++) {
+    console.log(`info of student #${i}`);
+    for (let j=0; j<student[i]. length; j++) {
+    console.log(student[i][j]);
+   }
 }
