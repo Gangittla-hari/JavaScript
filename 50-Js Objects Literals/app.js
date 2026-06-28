be68b607ffdcf15dbc48c16d7dfd75810eac117e
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-28 23:30:28 +0530

    added code

--- 50-Js Objects Literals/app.js
+++ 50-Js Objects Literals/app.js
@@ -1,5 +1,12 @@
-const student = {
-    name: "hari",
-    age: "23",
-    marks: 99.4
+// const student = {
+//     name: "hari",
+//     age: "23",
+//     marks: 99.4
+// };
+
+
+const item = {
+    price: 100.99,
+    discount: 70,
+    colors: ["red","pink"]
 };
