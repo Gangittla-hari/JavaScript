537b2ea902225e824feb171108cf3e8b95242b81
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-29 23:19:16 +0530

    added code

--- 51-Creating a post/app.js
+++ 51-Creating a post/app.js
@@ -6,5 +6,13 @@ const post = {
     tag: ["@hariprasd", "@oggy"]
 };
 
-//output
-//{username: '@hariprasad', content: 'This is my #firstPost', likes: 190, repost: 9, tag: Array(2)}
+//output 
+//{username: '@hariprasad', content: 'This is my #firstPost', likes: 190, repost: 9, tag: Array(2)}  
+
+const obj = {
+    1: "a",
+    2: "b",
+    true: "c",
+    null: "d",
+    undefined: "e"
+};
