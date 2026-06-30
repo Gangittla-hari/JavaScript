ab9d9777780af99e95c40f560475c08562095f3f
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-30 21:45:03 +0530

    Math Object

---
+++ 55-Math Object/app.js
@@ -0,0 +1,56 @@
+Math 
+Math {abs: ƒ, acos: ƒ, acosh: ƒ, asin: ƒ, asinh: ƒ, …}
+Properties -
+Math.PI
+Math.E
+Math.PI
+3.141592653589793
+Math.E
+2.718281828459045
+
+Methods
+Math.abs
+Math.abs(12);
+12
+Math.abs(-12);
+12
+Math.abs(-12.3);
+12.3
+
+//Math.pow
+// Math.pow(12,3);
+// 1728
+// Math.pow(2, 4);
+// 16
+// Math.pow(2,7);
+// 128
+
+//Math.floor
+// Math.floor(5)
+// 5
+// Math.floor(5.5)
+// 5
+// Math.floor(5.9999999999)
+// 5
+// Math.floor(-5)
+// -5
+// Math.floor(-5.5)
+// -6
+
+//Math.ceil
+// Math.ceil(-5.5)
+// -5
+// Math.ceil(5.5)
+// 6
+
+//Math.random
+// Math.random
+// ƒ random() { [native code] }
+// Math.random()
+// 0.8736845684433464
+// Math.random()
+// 0.03850301704618153
+// Math.random()
+// 0.6978692298035748
+// Math.random()
+// 0.023348017424645318
