30334637fed7d371904a34513b702a5bde11980f
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-30 17:59:22 +0530

    Add Update Value

---
+++ 52-Add-Update Values/app.js
@@ -0,0 +1,30 @@
+//Change the city to "Mumbai"
+//Add a new property, gender: "male"
+//Change the marks to "A"
+
+const student = {
+    name: "Hari",
+    age: "23",
+    marks: "94.4",
+    city: "bbsr"
+};
+
+
+// student
+// {name: 'Hari', age: '23', marks: '94.4', city: 'bbsr'}
+// student.city 
+// 'bbsr'
+// student.city = "Mumbai"
+// 'Mumbai'
+// student
+// {name: 'Hari', age: '23', marks: '94.4', city: 'Mumbai'}
+// student.gender
+// undefined
+// student.gender = "Male"
+// 'Male'
+// student
+// {name: 'Hari', age: '23', marks: '94.4', city: 'Mumbai', gender: 'Male'}
+// student.marks = "A"
+// 'A'
+// student
+// {name: 'Hari', age: '23', marks: 'A', city: 'Mumbai', gender: 'Male'}
