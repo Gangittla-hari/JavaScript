104b4ad3bc33afbb673e09a762ad81c2f638ea19
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-06-30 17:36:11 +0530

    added code

--- 51-Creating a post/app.js
+++ 51-Creating a post/app.js
@@ -5,10 +5,11 @@ const post = {
     repost: 9,
     tag: ["@hariprasd", "@oggy"]
 };
-
 //output 
 //{username: '@hariprasad', content: 'This is my #firstPost', likes: 190, repost: 9, tag: Array(2)}  
 
+//JS automatically converts objects keys to strings.
+//Even if we made the number as a key, the number will be converted to string.
 const obj = {
     1: "a",
     2: "b",
