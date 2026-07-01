2388e1afa7d256d72e8e868489071c8d441a32ff
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-01 18:49:28 +0530

    Assignment 4

---
+++ 59-Assignment 4/app.js
@@ -0,0 +1,23 @@
+//Ans 1
+let dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+
+//Ans 2
+const car = {
+    name: "Toyota",
+    model: "Camry",
+    year: 2020
+};
+console.log(car.name);
+
+//Ans 3
+const person = {
+    name: "jhon",
+    age: 30,
+    city: "New jersey",
+};
+
+person.country = "New York";
+person.country =  "United States";
+console.log(person);
