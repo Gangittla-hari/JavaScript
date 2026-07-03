04329d317a492b12c206cbda532b1cff82651889
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-03 21:11:14 +0530

    added code

--- 63-Practice Qa2/app.js
+++ 63-Practice Qa2/app.js
@@ -1,10 +1,17 @@
-// //Create a Function that gives us the average of 3 numbers ?.
+//Q1. Create a Function that gives us the average of 3 numbers ?.
 function calcAvg(a, b, c) {
     let avg = (a + b + c) / 3;
     console.log(avg);
 }
-//Call the function with 3 numbers.
+//Call the function 
 calcAvg(2, 4, 6);
 
 output: 4
 
+//Q2. Create a function that prints the multiplication table of a number ?.
+function printtable(n) {
+    for (let i=n; i<=n*10; i+=n) {
+        console.log(i);
+    }
+}
+printtable(5);
