adfeba62aa3394c380a725aa7b6b6d123b165b09
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-03 21:03:23 +0530

    Practice Questions 2

---
+++ 63-Practice Qa2/app.js
@@ -0,0 +1,10 @@
+// //Create a Function that gives us the average of 3 numbers ?.
+function calcAvg(a, b, c) {
+    let avg = (a + b + c) / 3;
+    console.log(avg);
+}
+//Call the function with 3 numbers.
+calcAvg(2, 4, 6);
+
+output: 4
+
