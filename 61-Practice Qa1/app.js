10bff19992be039101cdbe3635c9d9b89146b0ea
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-03 19:31:12 +0530

    Practice Questions

--- 61-Practice Qa1/app.js
+++ 61-Practice Qa1/app.js
@@ -8,3 +8,17 @@ function printpoem() {
 }
 
 printpoem();
+
+//Create a Function to roll a dice & always display  the value of the dice (1 to 6) ?.
+//Ex 1.
+let rand =  Math.floor(Math.random() * 6 ) +1;
+    console.log(rand);
+
+
+//Ex 2 of function to roll a dice & always display  the value of the dice (1 to 6).
+function rollDice() {
+    let rand =  Math.floor(Math.random() * 6 ) +1;
+    console.log(rand);
+}
+
+rollDice();
