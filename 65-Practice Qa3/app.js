310b875e24f731097a0ef0fb4d0cd47224b97dd3
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-04 22:43:55 +0530

    Practice Qa3

---
+++ 65-Practice Qa3/app.js
@@ -0,0 +1,9 @@
+//Q1. Create a Function that returns the sum of numbers from 1 to n.
+function getSum (n) {
+    let sum = 0;
+
+    for(let i=1; i<=n; i++){
+        sum += i;
+    }
+    return sum;
+}
