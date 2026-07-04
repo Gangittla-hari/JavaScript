ffb33837092c3c8f9dbcbf76f3169ea4e4cb1537
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-04 23:59:58 +0530

    added code

--- 66-Scope/app.js
+++ 66-Scope/app.js
@@ -3,7 +3,8 @@ let sum = 54;          //Global scope variable
 function calsum(a, b) {
     let sum = a + b;   //Function scope variable
     console.log(sum);
-}
+} 
 
+//Function call
 calsum(10, 20);
 console.log(sum);
