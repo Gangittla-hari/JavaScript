9cceb3d0ea45c71a41871cc50f6a0cefbee44a84
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-04 22:58:12 +0530

    added code

--- 65-Practice Qa3/app.js
+++ 65-Practice Qa3/app.js
@@ -1,20 +1,32 @@
 //Q1. Create a Function that returns the sum of numbers from 1 to n.
-function getSum (n) {
-    let sum = 0;
+// function getSum (n) {
+//     let sum = 0;
 
-    for(let i=1; i<=n; i++){
-        sum += i;
+//     for(let i=1; i<=n; i++){
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// //function call in the console
+// //output
+// getSum(4)
+// 10
+// getSum(3)
+// 6
+// getSum(2)
+// 3
+// getSum(5)
+// 15
+
+//Q2. Create a Function that returns the concatenation of all strings in an array.
+let str = ["Gangittla", "hariprasad", "is", "a", "good", "boy"];
+
+function concat(str) {
+    let result="";
+    
+    for(let i=0; i<str.length; i++) {
+        result += str[i];
     }
-    return sum;
+    return result;   
 }
-
-//function call in the console
-//output
-getSum(4)
-10
-getSum(3)
-6
-getSum(2)
-3
-getSum(5)
-15
