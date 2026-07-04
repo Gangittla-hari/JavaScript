d0fc68a1e611bf70819d485a6ecbb1d5cc0d550f
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-05 00:38:26 +0530

    Block Scope

---
+++ 67-Block  Scope/app.js
@@ -0,0 +1,20 @@
+{
+    let a = 24;
+}
+
+// console.log(a); 
+
+
+for(let i=1; i<=5; i++){
+    console.log(i); //block scope
+}
+
+// console.log(i);  
+
+let age = 25;
+if(age >= 18){
+    let str = "adult";
+    console.log(str);
+}
+
+// console.log(str);
