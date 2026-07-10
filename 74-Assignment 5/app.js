cf1e2f03d6e6ee758dc77e4ee47b5ab102a6ba56
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-10 13:50:20 +0530

    added code

--- 74-Assignment 5/app.js
+++ 74-Assignment 5/app.js
@@ -1,4 +1,4 @@
-//1.Ans
+//Ans 1.
 // function largerThanNumber(array, number) {
 //     let result = []
     
@@ -13,17 +13,36 @@
 
 
 //ex2 AC Ans
-let arr  = [8,9,10,1,2,3,4,5,6,7];
-let num = 5;
+// let arr  = [8,9,10,1,2,3,4,5,6,7];
+// let num = 5;
 //element larger then a number num
-function getElements(arr,num) {
-    for (let i=0; i< arr.length; i++) {
-        if (arr[i] > num) {
-            console.log(arr[i]);
-        }
+// function getElements(arr,num) {
+//     for (let i=0; i< arr.length; i++) {
+//         if (arr[i] > num) {
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+
+// getElements(arr,num); //call
+
+
+//Ans 2.
+let str = "abcdabcdefgggh";
+
+//function to get String with all unique elements
+function getUnique (str) {
+let ans = "";
+for (let i=0; i< str.length; i++) {
+    let currChar = str[i];
+    if (ans.indexOf(currChar) == -1) {
+        //if current character is not added, then add it in ans.
+        //Otherwise it is a duplicate.
+        ans += currChar;
     }
 }
+return ans;
+}
 
-getElements(arr,num); //call
-
+getUnique(str);
 
