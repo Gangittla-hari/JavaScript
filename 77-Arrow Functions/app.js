ca6152e13953b4737a8394cfb5910c00bee5e8c7
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-12 17:19:14 +0530

    Arrow Functions

---
+++ 77-Arrow Functions/app.js
@@ -0,0 +1,28 @@
+// //ex1.
+const sum = (a,b) => {
+    console.log(a+b);
+};
+// //output
+// // sum(2,2)
+// // sum(2,23)
+
+// //ex2.
+const cube = (n) => {
+    return n*n*n; //8
+};
+//output
+// cube(2)
+// 8
+// cube(4)
+// 64
+// cube(3)
+// 27
+
+
+//ex3.
+const pow = (a,b) => {
+    return a ** b;
+};
+//output
+//pow(2, 4);
+//16
