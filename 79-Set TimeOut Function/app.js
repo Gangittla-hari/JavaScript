c5939147e07ed1f8ebc95acac8e081af848b5ac4
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-12 18:02:10 +0530

    Set TimeOut Function

---
+++ 79-Set TimeOut Function/app.js
@@ -0,0 +1,13 @@
+console.log("Hi there!");
+
+setTimeout( () => {
+    console.log("Apna Collage");
+}, 4000);
+
+console.log("Welcome");
+
+
+//output
+Hi there!
+Welcome
+Apna Collage //4s ke bad a line run hoga
