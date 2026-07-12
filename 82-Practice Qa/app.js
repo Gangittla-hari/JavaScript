a7dd132e32b6eb365f906f9adfc735de273fdba8
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-12 21:27:51 +0530

    Added code

--- 82-Practice Qa/app.js
+++ 82-Practice Qa/app.js
@@ -1,6 +1,23 @@
 //Q1. Write an arrow function that returns the square  of a number 'n'.
-//Ans 1.
+//Ans
 const square = (n) => (n*n);
 
 console.log(square(4));
-//outpt is '16'
+// outpt is '16'
+
+
+
+
+//Q2. Write a function that prints "Hello World" 5 times at intervals of 2s each.
+//Ans
+let id = setInterval(() => {
+    console.log("Hello World");
+}, 2000);
+
+setTimeout(() => {
+    clearInterval(id);
+    console.log("Clear Interval ran");
+},10000);
+//output 
+//6 Hello World
+//Clear Interval ran
