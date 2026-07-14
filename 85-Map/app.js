ce8cd0cc16e6f314457d58c4bcfed6de647c4e3b
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-15 00:00:11 +0530

    added code

--- 85-Map/app.js
+++ 85-Map/app.js
@@ -1,5 +1,25 @@
-let num = [1,2,3,4];
+// let num = [1,2,3,4];
 
-let double = num.map((el)=>{
-    return el*2;
-})
+// let double = num.map((el)=>{
+//     return el*el;
+// })
+
+
+let students = [
+    {
+    name: "hari",
+    marks: 99,
+},
+{
+    name: "leo",
+    marks: 34,
+},
+{
+    name: "krio",
+    marks: 90.4,
+},
+];
+
+let gpa = students.map((el) => {
+    return el.marks / 10;
+});
