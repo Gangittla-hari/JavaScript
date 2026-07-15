e6aa336afc2257657d08400219aa1bbd46afdf55
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-16 00:35:04 +0530

    Reduce

---
+++ 86-Reduce/app.js
@@ -0,0 +1,15 @@
+let nums = [1,2,3,4];
+let finalVal = nums.reduce((res, el) => {
+    console.log(res);
+    return res + el
+});
+console.log(finalVal);
+//output 1
+// 1
+// 3
+// 6
+// 10 is finalValue
+
+
+Finding Maximum in an array 
+
