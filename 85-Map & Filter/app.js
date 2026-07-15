65aefb0f05d2cd63d52e8096d6f1f5980835fe29
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-15 23:41:34 +0530

    added code

--- 85-Map & Filter/app.js
+++ 85-Map & Filter/app.js
@@ -5,7 +5,7 @@ let double = num.map((el)=>{
     return el * 2; //double value store
 });
 
-
+//ex2
 let student = [
     {
         name: "hari",
@@ -26,4 +26,11 @@ let gpa = student.map((el) => {
 });
 
 
-let nums = []
+
+//Fliter 
+let nums = [1,2,3,4,5,6,7,8,9,10,11];
+let ans = nums.filter((el) => { //even -> true, odd -> false
+    return el % 2 == 0; // even num output (5) [2, 4, 6, 8, 10]
+    return el % 2 != 0; // odd num output (6) [1, 3, 5, 7, 9, 11]
+    return el < 5; //output is (4) [1, 2, 3, 4]
+});
