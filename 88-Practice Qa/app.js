3fe553b0db89c32c0d011a522a9c8b2481b31526
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-17 22:49:22 +0530

    added code

--- 88-Practice Qa/app.js
+++ 88-Practice Qa/app.js
@@ -1,18 +1,22 @@
-//Q1. Check if all numbers in our array are multiples of 10 or not.
+//Q1. Check if all numbers in our array are multiples of 10 or not. 
+
+//Ans.
 let nums = [10,20,30,40];
 
-let ans = nums.every((el) => el % 10 == 0);
+let ans = nums.every((el) => el % 10 == 0); 
 
-console.log(ans); //output is true
+console.log(ans); //output is true 
 
 
-//Q2. Create a function to find  the min number in an array.
+//Q2. Create a function to find  the min number in an array. 
+
+//Ans.
 let nums = [10,20,30,40,2]; 
 
 let min = nums.reduce((min, el) => {
    if (min < el) { 
-    return min; 
-   } else { 
+    return min;  
+   } else {  
     return el; 
    } 
 }); 
