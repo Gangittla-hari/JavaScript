aebd6077c21ee488aea4b052708a61e4faf3abeb
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-17 22:45:22 +0530

    added code

--- 88-Practice Qa/app.js
+++ 88-Practice Qa/app.js
@@ -7,38 +7,38 @@ console.log(ans); //output is true
 
 
 //Q2. Create a function to find  the min number in an array.
-let nums = [10,20,30,40,2];
+let nums = [10,20,30,40,2]; 
 
 let min = nums.reduce((min, el) => {
-   if (min < el) {
-    return min;
-   } else {
-    return el;
-   }
-});
+   if (min < el) { 
+    return min; 
+   } else { 
+    return el; 
+   } 
+}); 
 
-console.log(min);  //output is 2
+console.log(min);  //output is 2 
 
 
 
-//function
-function  getMin(nums) {
-   let min = nums.reduce((min, el) => {
-      if(min < el) {
-         return min;
-      } else {
-         return el;
+//function 
+function  getMin(nums) { 
+   let min = nums.reduce((min, el) => { 
+      if(min < el) { 
+         return min; 
+      } else { 
+         return el; 
       }
-   });
-   return min;
-}
+   }); 
+   return min; 
+} 
 
-let nums = [10,20,30,40,2];
+let nums = [10,20,30,40,2]; 
 
-//output
+//output.
 getMin(nums);
-2
-getMin([1,2,4,5,6]);
-1
+2 
+getMin([1,2,4,5,6]); 
+1 
 getMin([1,2,4,5,6,-1]);
 -1
