f46e33d323fae3db67b347e9ed227f6c8864bf54
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-20 21:40:50 +0530

    added code

--- 92-Spread (Object Literals)/app.js
+++ 92-Spread (Object Literals)/app.js
@@ -3,4 +3,9 @@ const  data = {
     password: "abcd",
 };
 
-const datacopy = {...data, id: 123};
+const datacopy = {...data, id: 123, country: "india"};
+
+let arr = [1,2,3,4,5]; //val
+let obj1 = { ...arr}; //obj -> key:val
+
+let obj2 = {..."hello"}; //string
