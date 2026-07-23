a387206184874ecc4d7e475c435cf46585c081e7
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-23 21:10:16 +0530

    added code

--- 96-Assignment 7/app.js
+++ 96-Assignment 7/app.js
@@ -9,3 +9,7 @@ let sum = square.reduce((acc, cur) => acc + cur, 0);
 let avg = sum / nums.length;
 console.log(evg);
 
+//Ans 2
+let numbers = [2,4,6,8,-2,-4];
+
+console.log(numbers.map((number) => number + 5)); //output (6) [7, 9, 11, 13, 3, 1]
