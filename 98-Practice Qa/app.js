828a256e84773fedf39a0ef95958de7a28947107
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-07-31 00:01:16 +0530

    added code

--- 98-Practice Qa/app.js
+++ 98-Practice Qa/app.js
@@ -8,3 +8,26 @@ para1.innerText = "Hey I'm red!";
 document.querySelector("body").append(para1);
 
 para1.classList.add("red");
+
+
+//ans2.
+let h3 = document.createElement("h3");
+h3.innerText = "I'm a blue h3!";
+document.querySelector("body").append(h3);
+
+h3.classList.add("blue");
+
+
+//ans3.
+let div = document.createElement("div");
+let h1 = document.createElement("h1");
+let para2 = document.createElement("p");
+
+h1.innerText = "I'm in a div";
+para2.innerText = "ME TOO!";
+
+div.append(h1);
+div.append(para2);
+div.classList.add("box");
+
+document.querySelector("body").append(div);
