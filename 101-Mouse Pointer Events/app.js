bf006cba9a32471de1814b7bcf7770500f074eeb
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-08-05 16:33:20 +0530

    added code

--- 101-Mouse Pointer Events/app.js
+++ 101-Mouse Pointer Events/app.js
@@ -1,7 +1,25 @@
-let btn = document.querySelector('button');
-console.dir(btn);
+//onclick event
 
-btn.onclick = function() {
-    // console.log('Button  was clicked');
-    alert('Button was clicked');
+// let btn = document.querySelector('button');
+// console.dir(btn);
+
+// btn.onclick = function() {
+//     // console.log('Button  was clicked');
+//     alert('Button was clicked'); 
+// }
+
+
+let  btns = document.querySelectorAll('button');
+
+for (btn of btns) {
+    btn.onclick = sayHello;
+    btn.onmouseenter = function() { //onmouseenter event
+        console.log("you entered a button") //this event is triggered when the mouse pointer enters the button
+    }
+    console.log(btn);
 }
+
+function sayHello() {
+    alert('Hello');
+}
+
