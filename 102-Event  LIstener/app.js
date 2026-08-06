d3160f9435837b59beb00b75bfe84731572d2333
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-08-06 20:17:10 +0530

    added code

--- 102-Event  LIstener/app.js
+++ 102-Event  LIstener/app.js
@@ -1,18 +1,21 @@
 let btns = document.querySelectorAll('button');
 
 for (btn of btns) {
-    // btn.onclick = sayHello;
-    // btn.onclick = sayName;
+//    btn.onclick = sayHello;                  
+//    btn.onclick = sayName;
 
-    btn .addEventListener("click", sayHello);
-     btn .addEventListener("click", sayName);
+// btn.addEventListener('click', sayHello);    // click event listener
+// btn.addEventListener('click', sayName);
+btn.addEventListener('dblclick', function() {  //doucle click event listener 
+console.log("you double clicked me");
+});
 }
 
+
 function sayHello() {
-    alert('Hello');
+   alert('Hello');
 }
 
 function sayName() {
-    alert('My name is hari');
+    alert("apna college");
 }
-
