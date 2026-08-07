81edcb6d96497dc2edfa09e6cb6b080dd4f1c8a0
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-08-07 23:22:33 +0530

    added code

--- 106-Keyboard Events/app.js
+++ 106-Keyboard Events/app.js
@@ -1,36 +1,36 @@
-// let input = document.querySelector("input");
+let input = document.querySelector("input");
 
-// input.addEventListener("keydown", function(event) {
-//     console.log("key= ", event.key);
-//     console.log("code=",event.code);
-//     console.log("key was pressed"); 
-// });
+input.addEventListener("keydown", function(event) {
+    console.log("key= ", event.key);
+    console.log("code=",event.code);
+    console.log("key was pressed"); 
+});
 
 
 // ex2.
-// let btn = document.querySelector("button");
-// btn.addEventListener("click", function(event) {
-//     console.log(event);
-//     console.log("button clicked");
-// });
+let btn = document.querySelector("button");
+btn.addEventListener("click", function(event) {
+    console.log(event);
+    console.log("button clicked");
+});
 
-// btn.addEventListener("dblclick", function(event) {
-//     console.log(event);
-//     console.log("button clicked");
-// });
+btn.addEventListener("dblclick", function(event) {
+    console.log(event);
+    console.log("button clicked");
+});
 
 
 
 //ex3.
-// let input =  document .querySelector("input");
+let input =  document .querySelector("input");
 
-// input.addEventListener("keydown", function() {
-//  console.log("key was pressed"); 
-// }); 
+input.addEventListener("keydown", function() {
+ console.log("key was pressed"); 
+}); 
 
-// input.addEventListener("keyup", function() {
-//     console.log("key was released"); 
-// }); 
+input.addEventListener("keyup", function() {
+    console.log("key was released"); 
+}); 
 
 
 
@@ -49,3 +49,4 @@ input.addEventListener("keydown", function(event) {
         console.log("character moved right");
     }
 });
+
