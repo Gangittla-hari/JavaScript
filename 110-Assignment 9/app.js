7bde5b447ae63aa4729889d03977427d7d37af15
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-08-10 13:55:04 +0530

    added code

--- 110-Assignment 9/app.js
+++ 110-Assignment 9/app.js
@@ -1,49 +1,31 @@
-//Mouseout 1.
-// const test = document.getElementById("test");
-
-// test.addEventListener("mouseleave", (event) => {
-//     event.target.style.color = "purple";
-
-//     setTimeout(() => {
-//         event.target.style.color = "";
-//     }, 1000);
-// });
-
-// test.addEventListener("mouseout", (event) => {
-//     event.target.style.color = "orange";
-
-//     setTimeout(() => {
-//         event.target.style.coloe = "";
-//     }, 500);
-// });
-
 //Ans 1.
-//Mouseout 2
-// let box = document.querySelector("#box");
+//Mouseout 
+let box = document.querySelector("#box");
 
-// box.addEventListener("mouseout", function() {
-//     console.log("mouse moved out!");
-// });
+box.addEventListener("mouseout", function() {
+    console.log("mouse moved out!");
+});
 
 
 //keypress
-// let keypress = document.querySelector("keypress");
+let keypress = document.querySelector("keypress");
 
-// document.addEventListener("keypress", function (event) {
-//     console.log("Key pressed:", event.key);
-// });
+document.addEventListener("keypress", function (event) {
+    console.log("Key pressed:", event.key);
+});
 
 
 //Scroll
-// window.addEventListener("scroll", function() {
-//     console.log("Page is scrolling")
-// })
+window.addEventListener("scroll", function() {
+    console.log("Page is scrolling")
+})
 
 
 //load
-// window.addEventListener("load", function() {
-//     console.log("Page loaded successfully!");
-// });
+window.addEventListener("load", function() {
+    console.log("Page loaded successfully!");
+});
+
 
 //Ans 2.
 let btn = document.createElement("button");
@@ -53,4 +35,18 @@ document.querySelector("body").append(btn);
 
 btn.addEventListener("click", function() {
     btn.style.backgroundColor = "green";
+});
+
+
+//Ans 3.
+let input =  document.querySelector("#name");
+let h2 = document.querySelector("h2");
+
+input.addEventListener("input", function () {
+    let value = input.value;
+
+    let onlyLetters = value.replace(/[^a-zA-Z]/g, "");
+
+    input.value = onlyLetters;
+    h2.innerText = onlyLetters;
 });
