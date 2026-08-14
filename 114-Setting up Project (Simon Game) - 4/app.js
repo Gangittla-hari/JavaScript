d5d5e3b64fee5d945cf1c67e33c19ab60186e484
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-08-14 22:55:03 +0530

    added code

--- 114-Setting up Project (Simon Game) - 4/app.js
+++ 114-Setting up Project (Simon Game) - 4/app.js
@@ -1,8 +1,7 @@
-let gameseq=[];
-let userseq=[];
+let gameSeq=[];
+let userSeq=[];
 
 let btns = ["yellow", "red", "purple", "green"];
-
 
 let started = false;
 let level = 0;
@@ -38,16 +37,28 @@ function levelUp() {
     let randIdx = Math.floor(Math.random() * 3);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
-    console.log(randIdx);
-    console.log(randColor);
-    console.log(randBtn);
+    // console.log(randIdx);
+    // console.log(randColor);
+    // console.log(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
     gameFlash(randBtn);
 }
+
+function checkAns() {
+    console.log("curr level : ", level);
+}
+
 
 function btnPress() {
    console.log(this);
    let btn = this;
    userFlash(btn);
+
+   userColor = btn.getAttribute("id");
+   userSeq.push(userColor);
+
+   checkAns();
 }
 
 let allBtns = document.querySelectorAll(".btn");
