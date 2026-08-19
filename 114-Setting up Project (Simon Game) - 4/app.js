8a738cf339b01bb5ac69959f2fe30df09aff3cc5
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-08-19 17:19:59 +0530

    added code

--- 114-Setting up Project (Simon Game) - 4/app.js
+++ 114-Setting up Project (Simon Game) - 4/app.js
@@ -1,6 +1,8 @@
 let gameSeq=[];
 let userSeq=[];
 
+let btns = ["yellow", "red", "purple", "green"];
+
 let started = false;
 let level = 0;
 
@@ -15,7 +17,50 @@ document.addEventListener("keypress", function () {
     }
 });
 
+function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 250);;
+}
+
+function userFlash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(function () {
+        btn.classList.remove("userflash");
+    }, 250);;
+}
+
 function levelUp() {
     level++;
     h2.innerText = `Level ${level}`;
+
+
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btns [randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    // console.log(randIdx);
+    // console.log(randColor);
+    // console.log(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randBtn);
+}
+
+function checkAns() {
+    console.log("curr  level : ", level);
+}
+
+function btnPress() {
+    console.log(this);
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns) {
+    btn.addEventListener("click", btnPress);
 }
