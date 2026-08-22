7940815fc9fc17b37f392f9cad2edd4d2470f4ea
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-08-22 19:27:03 +0530

    call stack

--- 114-Simon-Says-Game - 4/app.js
+++ 114-Simon-Says-Game - 4/app.js
@@ -21,14 +21,14 @@ function gameFlash(btn) {
     btn.classList.add("flash");
     setTimeout(function () {
         btn.classList.remove("flash");
-    }, 250);;
+    }, 250);
 }
 
 function userFlash(btn) {
     btn.classList.add("userflash");
     setTimeout(function () {
         btn.classList.remove("userflash");
-    }, 250);;
+    }, 250);
 }
 
 function levelUp() {
@@ -49,15 +49,15 @@ function checkAns(idx) {
     if(userSeq[idx] === gameSeq[idx]) {
         if(userSeq.length ==  gameSeq.length) {
             setTimeout(levelUp, 1000);
-        }
-    } else {
-        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to start.`;
-        document.querySelector("body").style.backgroundColor= "red";
-        setTimeout(function () {
-             document.querySelector("body").style.backgroundColor = "white";
-        });
-        reset();
-    }
+        } 
+    } else { 
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to start.`; 
+        document.querySelector("body").style.backgroundColor= "red"; 
+        setTimeout(function () { 
+             document.querySelector("body").style.backgroundColor = "white"; 
+        }); 
+        reset(); 
+    } 
 }
 
 function btnPress() {
