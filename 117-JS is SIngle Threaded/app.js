0aff504804c02bd9b3ac6bde0c584fd63f7b9b6f
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-08-25 13:43:36 +0530

    added code

---
+++ 117-JS is SIngle Threaded/app.js
@@ -0,0 +1,20 @@
+function one() {
+    return 1;
+}
+
+function two() {
+    return one() + one();
+}
+
+function three() {
+    let ans = two() + one();
+    console.log(ans);
+}
+
+three();                    
+
+
+
+
+
+        
