62204dc692ba9d5bee58fab9a65840e4ffa06cb6
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-08-26 14:07:57 +0530

    added code

--- 118-Callback Hell/app.js
+++ 118-Callback Hell/app.js
@@ -1,12 +1,28 @@
 h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColorchange) {
+function changecolor(color, delay, nextColorChange) {
     setTimeout(() => {
         h1.style.color = color;
+        if (nextColorChange) nextColorChange();
     }, delay);
-    h1.style.color = color;
 }
 
-changeColor("red", 1000);
-changeColor("orange", 2000);
-changeColor("green", 3000);
+changecolor("red", 1000, () => {
+    changecolor("orange", 1000, () => {
+        changecolor("green", 1000);
+    });
+});
+
+
+
+
+// function changeColor(color, delay, nextColorchange) {
+//     setTimeout(() => {
+//         h1.style.color = color;
+//     }, delay);
+//     h1.style.color = color;
+// }
+
+// changeColor("red", 1000);
+// changeColor("orange", 2000);
+// changeColor("green", 3000);
