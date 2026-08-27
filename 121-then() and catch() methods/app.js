f68fa2c6cefd1bb84345b0f931fc4d9974e917bf
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-08-27 19:56:38 +0530

    then() and catch() methods

--- 121-then() and catch() methods/app.js
+++ 121-then() and catch() methods/app.js
@@ -1,13 +1,20 @@
 function savetoDb(data) {
   return new Promise((resolve, reject) => {
-    let internetSpeed = Math.floor(Math.random() * 10) +1;
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
     if (internetSpeed > 4) {
       resolve("success : data was saved");
     } else {
-      reject("failure : week connection");
+      reject("failure : weak connection");
     }
   });
 }
 
 let request = savetoDb("apna college"); //req = promise object
-console.log
+request.then(() => {
+    console.log("promise was resolved");
+    console.log(request);
+})
+.catch(() => {
+    console.log("promise was rejected");
+    console.log(request);
+});
