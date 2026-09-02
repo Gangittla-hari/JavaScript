35c82ae0eb62a932f2aeb7aa1f31be396bed2d6e
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-09-02 22:17:54 +0530

    added code

--- 127-Handing Rejections/app.js
+++ 127-Handing Rejections/app.js
@@ -1,23 +1,25 @@
-h1 = document.querySelector("h1");
+h1 = document.querySelector("h1"); 
+ 
+function changeColor(color, delay) { 
+    return new Promise((resolve, reject) => { 
+        setTimeout(() => { 
+            let num = Math.floor(Math.random() * 5) + 1;        
+            if (num > 3) { 
+                reject("promise rejected"); 
+            } 
+            h1.style.color = color; 
+            console.log(`color changed to ${color}!`); 
+            resolve("color changed!"); 
+        }, delay); 
+    }); 
+} 
 
-function changeColor(color, delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let num = Math.floor(Math.random() * 5) + 1;
-            if (num > 3) {
-                reject("promise rejected");
-            }
-            h1.style.color = color;
-            console.log(`color changed to ${color}!`);
-            resolve("color changed!");
-        }, delay);
-    });
-}
+
 
 async function demo() {
 try {
     await changeColor("red", 1000);
-    await changeColor("orange", 1000);
+    await changeColor("orange", 1000);                          
     await changeColor("yellow", 1000);
     await changeColor("blue", 1000);
 }   catch (err) {
@@ -27,5 +29,12 @@ try {
 
     let a = 5;
     console.log(a);
-    console.log("new number = ", a + 3);
+    console.log("new number = ", a + 3);                            
 }
+ 
+
+
+
+
+
+
