7099afa73eb5b43fa71cb3552671d56159a5c782
Gangittla hari prasad <hariprasad20987@gmail.com>
2026-09-15 16:49:36 +0530

    add ss

--- 133-Sending Headers with API requests/app.js
+++ 133-Sending Headers with API requests/app.js
@@ -2,7 +2,8 @@ const url = "https://icanhazdadjoke.com/";
 
 async function getJokes() {
     try {
-        let res = await axios.get(url);
+        const config = { headers: { Accept: "application/json" } };
+        let res = await axios.get(url, config);
         console.log(res.data);
     } catch (err) {
         console.log(err);
